17386df0ef15d329fd184c79c56c18d883c48e20
Yezzel <kyeibenjamin03@gmail.com>
2025-03-19 14:22:07 +0000

    My responsive portfolio website

---
+++ script/main.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('.header .fa-bars-staggered');
+let navbar = document.querySelector('.header .navbar');
+
+
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
+    
+}
+
